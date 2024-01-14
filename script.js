94c9a4a95c80decae259ac4fbe29a385d3658303
AyushKumar015 <kumar.ayush15102002@gmail.com>
2024-01-14 12:17:56 +0530

    few lines added

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// calculator
+
+let a = document.getElementById("display");
+
+function call(value) 
+{
+    if(value=='ac')
+    {
+        a.value ='';
+    }
+    
+    else if(value=='=')
+    {
+        a.value=eval(display.value)
+    }
+    else
+    a.value += value;
+}
+
+
 
